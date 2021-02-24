d1131783eec960b4ab553d492cf8ca9fa9dc31aa
diegorom27 <diegorom27@gmail.com>
2021-02-23 20:43:59 -0500

    .

--- src/estadisticaProyecto/EstadisticaProyecto.java
+++ src/estadisticaProyecto/EstadisticaProyecto.java
@@ -14,11 +14,6 @@ import java.util.Scanner;
  */
 public class EstadisticaProyecto {
     
-    
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
